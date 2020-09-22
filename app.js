cb1bdc726e1d581043bcdde16ae605e1ecb78db1
Connor <connorskorburg@gmail.com>
2020-09-22 14:25:47 -0500

    updated styling on mobile

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ document.getElementById('hamburger').addEventListener('click', () => {
 document.getElementById('close').addEventListener('click', () => {
   document.getElementById('mobile-nav').style.display = 'none';
 })
+
+const btns = document.querySelectorAll('#mobile-nav section a');
+
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.getElementById('mobile-nav').style.display = 'none';
+  })
+});
