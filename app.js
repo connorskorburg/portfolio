c25f2a1da296ca323fa9aab89c11438f47787565
Connor <connorskorburg@gmail.com>
2020-09-21 19:53:30 -0500

    added nav toggler for mobile layout

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('hamburger').addEventListener('click', () => {
+  document.getElementById('mobile-nav').style.display = 'block';
+});
+
+document.getElementById('close').addEventListener('click', () => {
+  document.getElementById('mobile-nav').style.display = 'none';
+})
